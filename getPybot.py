68906e4a8f056518bd68ffae31c3137ce4c8764a
auwsome <auwsome@users.noreply.github.com>
2015-08-24 10:02:20 -0700

    Update getPybot.py

--- getPybot.py
+++ getPybot.py
@@ -1,25 +1,31 @@
 # from https://raw.githubusercontent.com/auwsome/pybot/master/getPybot.py
 
-import os, urllib2
+import os, sys, urllib2
 
 realcwd = os.path.dirname(os.path.realpath(__file__))
 
-url1 = 'http://raw.githubusercontent.com/auwsome/pybot/master/Pybot.py'
-#urls[1] = 'http://raw.githubusercontent.com/auwsome/pybot/master/sendPybot.py'
-#for url in urls[url]:
+urls[0] = 'http://raw.githubusercontent.com/auwsome/pybot/master/Pybot.py'
 
-req = urllib2.Request(url1)
-response = urllib2.urlopen(req)
-# print response.getcode()
-# print response.headers.getheader('content-type
-try:
+if sys.platform.system() == 'windows': urls[1] = 'https://raw.githubusercontent.com/auwsome/pybot/master/pluginWindows.py'
+if os.name == 'android': urls[1] = 'https://raw.githubusercontent.com/auwsome/pybot/master/pluginAndroid.py'
+if os.name == 'posix': urls[1] = 'https://raw.githubusercontent.com/auwsome/pybot/master/plugin .py'
+if sys.platform.system() == 'Linux': urls[1] = 'https://raw.githubusercontent.com/auwsome/pybot/master/plugin .py'
+
+print os.name, sys.platform.system(), sys.platform.release()
+
+for url in urls[url]:
+	req = urllib2.Request(url)
 	response = urllib2.urlopen(req)
-except urllib2.HTTPError, e:
-	print('HTTP Error', e)
-	res = e.fp.read()
-	#return json.loads(res), str(e)
-page = response.read()
-#print json.loads(res)
+	# print response.getcode()
+	# print response.headers.getheader('content-type
+	try:
+		response = urllib2.urlopen(req)
+	except urllib2.HTTPError, e:
+		print('HTTP Error', e)
+		res = e.fp.read()
+		#return json.loads(res), str(e)
+	page = response.read()
+	#print json.loads(res)
 
 outfile = os.path.join(realcwd,'Pybot.py')
 with open(outfile, 'w') as file:
