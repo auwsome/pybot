017ef576b73a4327caea872aa8e178f795660b44
auwsome <auwsome@users.noreply.github.com>
2015-08-24 11:28:10 -0700

    Update getPybot.py

--- getPybot.py
+++ getPybot.py
@@ -4,18 +4,18 @@ import os, sys, urllib2
 
 realcwd = os.path.dirname(os.path.realpath(__file__))
 
-urls=[0]*2; 
-urls[0] = 'http://raw.githubusercontent.com/auwsome/pybot/master/Pybot.py'
+#urls=[0]*2; 
+urls={}; urls['pluginGitHub.py'] = 'http://raw.githubusercontent.com/auwsome/pybot/master/Pybot.py'
 
-if sys.platform == 'win32'  : urls[1] = 'https://raw.githubusercontent.com/auwsome/pybot/master/pluginWindows.py'
-if sys.platform == 'android': urls[1] = 'https://raw.githubusercontent.com/auwsome/pybot/master/pluginAndroid.py'
-if sys.platform == 'darwin' : urls[1] = 'https://raw.githubusercontent.com/auwsome/pybot/master/plugin .py'
-if sys.platform == 'linux2' : urls[1] = 'https://raw.githubusercontent.com/auwsome/pybot/master/plugin .py'
+if sys.platform == 'win32'  : urls['pluginWindows.py'] = 'https://raw.githubusercontent.com/auwsome/pybot/master/pluginWindows.py'
+if sys.platform == 'android': urls['pluginAndroid.py'] = 'https://raw.githubusercontent.com/auwsome/pybot/master/pluginAndroid.py'
+if sys.platform == 'darwin' : urls['plugin.py'] = 'https://raw.githubusercontent.com/auwsome/pybot/master/plugin .py'
+if sys.platform == 'linux2' : urls['plugin.py'] = 'https://raw.githubusercontent.com/auwsome/pybot/master/plugin .py'
 
 print os.name, sys.platform
 
-for i,url in enumerate(urls):
-	req = urllib2.Request(url)
+for url in urls:
+	req = urllib2.Request(urls[url])
 	response = urllib2.urlopen(req)
 	# print response.getcode()
 	# print response.headers.getheader('content-type
@@ -27,8 +27,7 @@ for i,url in enumerate(urls):
 		#return json.loads(res), str(e)
 	page = response.read()
 	#print json.loads(res)
-
-outfile = os.path.join(realcwd,'Pybot.py')
-with open(outfile, 'w') as file:
-	file.write(page)
-print outfile
+	outfile = os.path.join(realcwd,url)
+	with open(outfile, 'w') as file:
+		file.write(page)
+	print outfile
