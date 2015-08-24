609f31d49c5512343e9cd281fa46327826eade09
auwsome <auwsome@users.noreply.github.com>
2015-08-23 21:53:38 -0700

    Update getPybot.py

--- getPybot.py
+++ getPybot.py
@@ -1,19 +1,27 @@
-
+# from https://raw.githubusercontent.com/auwsome/pybot/master/getPybot.py
 
 import os, urllib2
 
 realcwd = os.path.dirname(os.path.realpath(__file__))
 
-urls[0] = 'http://raw.githubusercontent.com/auwsome/pybot/master/Pybot.py'
+url1 = 'http://raw.githubusercontent.com/auwsome/pybot/master/Pybot.py'
+#urls[1] = 'http://raw.githubusercontent.com/auwsome/pybot/master/sendPybot.py'
+#for url in urls[url]:
 
-for url in urls[url]:
-	req = urllib2.Request(url1)
+req = urllib2.Request(url1)
+response = urllib2.urlopen(req)
+# print response.getcode()
+# print response.headers.getheader('content-type
+try:
 	response = urllib2.urlopen(req)
-	# print response.getcode()
-	# print response.headers.getheader('content-type')
-	page = response.read()
+except urllib2.HTTPError, e:
+	print('HTTP Error', e)
+	res = e.fp.read()
+	#return json.loads(res), str(e)
+page = response.read()
+#print json.loads(res)
 
-	outfile = os.path.join(realcwd,'temp.py')
-	with open(outfile, 'w') as file:
-		file.write(page)
-	print realcwd+'temp.py'
+outfile = os.path.join(realcwd,'Pybot.py')
+with open(outfile, 'w') as file:
+	file.write(page)
+print outfile
