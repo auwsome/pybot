a5cb11d633781582edc22b53dbd8e5ff68617e8b
auwsome <abc@gmail.com>
2015-08-28 08:00:32 -0700

    s

--- Pybot.py
+++ Pybot.py
@@ -1,12 +1,8 @@
 # from https://github.com/auwsome/pybot
-print 1
 import os, sys, subprocess
-print 2
 import csv, json
 #import threading
-print 1b
 import android
-print 2b
 # try:
 	# import serverSCP as s
 # except ImportError,e: print str(e)
