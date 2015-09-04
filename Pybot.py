0766de06b94513fb2151c8c1c8514fb0f22dac64
auwsome <abc@gmail.com>
2015-09-04 08:24:56 -0700

    b

--- Pybot.py
+++ Pybot.py
@@ -1,11 +1,16 @@
 # from https://github.com/auwsome/pybot
-import os, sys, subprocess
-import csv, json
+print 'init'
+import os, sys, subprocess, time
+import csv, json, urllib2
+import mechanize
+import bs4
 #import threading
-import android
 # try:
 	# import serverSCP as s
 # except ImportError,e: print str(e)
+
+
+
 
 # vars
 global realcwd; realcwd = os.path.dirname(os.path.realpath(__file__))
@@ -13,28 +18,36 @@ global cwd; cwd = os.getcwd()
 #print 'path= ' realcwd
 global dctn; dctn={'is':'equals', 'thing':'something', 'quit':'end loop', 'how':'thing', '?':'question mark', ' ':'space'}
 global commands; commands = []
-global input; input = None
+global input; input = "hi"
 global args; args = None
 global context; context = None
-
+global prompt; prompt = 'test'
+global channel; channel = 'raw_input('+prompt+'<>)'
 print sys.platform
 global dictName, cmdsName
 global serverCheck; serverCheck = 0
+global cmdline; 
 if sys.platform == 'win32': 
 	dictName = realcwd+'\dict.json'
 	cmdsName = realcwd+'\commands.json'
 	#### command line have to prefix running script with 'python'
-	try: 
-		args = sys.argv[1:] 
-		input = " ".join(args)
-		#print input, words, sys.argv[0:], len(sys.argv)
-	except Exception,e: print str(e)
-	global cmdline; 
-	if args: cmdline=1
-if sys.platform == 'android': 
+	
+
+
+
+global droid, d, tts 
+######## check and set environment
+if True or 'arm' in sys.platform:# == 'linux-armv71': 
 	dictName = '/storage/sdcard1/dict.json'
 	cmdsName = '/storage/sdcard1/commands.json'
-	import pluginAndroid as droid
+	import android 
+	from BeautifulSoup import BeautifulSoup
+	droid = android.Android(); d = droid
+	tts = True
+	channel = 'd.ttsSpeak("yes?"); input = droid.recognizeSpeech(None,None,None).result'
+	args = droid.getIntent().result[u'extras']
+	print args
+	if args: input = args['%avcomm']; print input
 
 #### server, ie Cortana
 if serverCheck==1:
@@ -43,31 +56,43 @@ if serverCheck==1:
 		input = str(s.listen())
 		print 'done'
 	except Exception,e: print 'no server\n'+str(e)
+
+if sys.argv[1:]:
+	try: 
+		args = sys.argv[1:]
+		if args: cmdline=1
+		print args
+		input = " ".join(args)
+		print input, sys.argv[0:], len(sys.argv)
+	except Exception,e: print str(e)
+
+#input=""
+print input
+def main(input=input, *args):
+	response='hi'; choose=False; choice="go"; YorN=None; words = ['']; chunk=0; link=0
+	global droid, prompt
 	
-	
-def main(*args):
-	response='hi';input='';choice=None;YorN=None
 	#### MAIN LOOP:
-	while response is not None:
+	while response is not "":
+	
 		################### input and convert to list of words
+		print 'input1='+input, "response1="+response #, "choice="+choice
 		
-		#### text input
-		if not response: input = raw_input('>')
-		#print 1
-		if response: input = raw_input(response+'\n>')
-		if choice: choice = raw_input('choose>')
-		if YorN: YorN = raw_input(response+' (y/n)\n>')
-		#if input == 'y' or 'yes': input = response
-		#print 2
+		while input == "" or input == 'nospeech' or input is None:
+			input = droid.recognizeSpeech().result
+			if not response: print 'noresponse'; input = droid.recognizeSpeech().result#exec(channel)
+			if choose: print 'choose'; prompt = choice; choice = droid.recognizeSpeech().result; input="choose"#exec(channel)
+			if not choose and response: input = droid.recognizeSpeech().result # prompt = response+'>'; exec(channel)
+			
+			if input is None: time.sleep(7);  input="";  #print 2 #exec(channel)
+			else: print "input2=",input;
+		 
+		#exec('print 2')
 		# if input is None: 
 			# prompt = response+'>'
 			# input = raw_input('>')
 		try: words = input.split(' ')
 		except: pass
-		if sys.platform == 'linux-armv71':
-			print 'speaking'
-		droid=android.Android()
-		droid.ttsSpeak(response) 
 		
 		#### set context(s)
 		'''if context: 
@@ -78,16 +103,19 @@ def main(*args):
 			#if confirm == 'y':  context = confirm; context = None; input ="okay"'''
 		
 		################### direct commands
-		if input == 'quit': response = None	
+		if input == 'quit': response = ""
 		if input == 'save': PBcreateBranch(); break
 		if input == 'dctn': response = str(dctn); print response, dctn; continue
-		if 'hi' in input: response = 'hello'
+		if input == "hi": response = 'hello'
+		if prompt == 'anything else? (yes/no)>':
+			if YorN == 'yes': pass
+			if YorN == 'no': break
 		
 		################### keyword based commands
 		
 		########## definitions
-		if 'is' in input and not 'what is' in input and not words[0] == 'is': 
-			df= input.split(' is ') #definition 
+		if ' is ' in input and not 'what is ' in input and not words[0] == 'is': 
+			df = input.split(' is ') #definition 
 			try: dctn[df[0]] = df[1]
 			except: print 'error, not entered' #dctn[df[0]]=[df[1]]
 			if df[1] == 'action':
@@ -97,7 +125,7 @@ def main(*args):
 			response = 'okay'
 			#continue
 			
-		if 'is not' in input: 
+		if ' is not ' in input: 
 			split= input.split(' is not ') #remove definition 
 			try: dctn[split[0]].remove(split[1])
 			except: pass
@@ -120,17 +148,37 @@ def main(*args):
 			from pygoogle import pygoogle
 			g = pygoogle(query)
 			g.pages = 1
-			results = g.__search__()
-			response = str(results)
-			choose=True
-		if choice:
-			print choice
-			if choice == 'next'		: print 1
-			if choice == 'cancel'	: print 2
-			# if choice == 'next': print 1
-			# if choice == 'next': print 1
+			results = g.__search__(); #print str(results)
+			choose=True; 
+			response = results[link]['content']; #response = repr(response)
+			response.encode('ascii')
+			#response.encode('ascii', 'ignore'); 
 			
-		
+		##################################################################################################################################	
+		if choose:
+			print 'chooseTrue'
+			if choice == 'next': 
+				link=link+1; print 'link=',link
+				response = results[link]['content']; #response = repr(response)
+				response.encode('ascii')
+			if choice == 'go': 
+				br = mechanize.Browser()
+				br.set_handle_robots(False)
+				br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]  
+				page = br.open(url); 
+				response = page.read()
+				soup = BeautifulSoup(response, "html.parser"); 
+				#paras=soup.p #findAll('p', text=True)
+				VALID_TAGS = ['p','span']		#, 'ul', 'li', 'br']'div',
+				paras = [i.text.encode('ascii',"ignore") for i in soup.find_all(VALID_TAGS)] ################## removes <p>s
+				paras = filter(None, paras)
+				paras = [i.replace('\n','.').replace('\r','.') for i in paras] 
+				paras = [i.replace('(','parens').replace(')','parens').replace('[','bracket').replace(']','bracket') for i in paras] 
+			
+				input = raw_input('pause')
+			
+			
+			
 		######## actions
 		if 'e' in input:
 			exec1 = input.split('e ') #exec
@@ -141,7 +189,8 @@ def main(*args):
 			try: 
 				exec(dctn[words[1]]['action']+' "'+str(''.join(words[2:99]))+'"'); continue
 			except Exception,e: print str(e)
-			
+		
+		print 'words=',words	
 		if words[0] and words[0] in commands:
 			try: 
 				vcommand = words[0]
@@ -163,20 +212,60 @@ def main(*args):
 			continue
 		
 		########################### catch unknown words
-		for word in words:
-			word.strip('?')
-			if word not in dctn and 'is' not in input:  response = 'what is '+ word +"?"
+		# if words[0]:
+			# for word in words:
+				# word.strip('?')
+				# if word not in dctn and 'is' not in input:  response = 'what is '+ word +"?"
 			
-		########################### output
-		#if response: print response	
-		if response is None: YorN = raw_input('anything else?')
-		if YorN == 'y': response = input
-		print "input="+input
 		
+		
+		########################### output
+		if response: print 'endresponse=',response
+		else: prompt = 'anything else? (yes/no)>'
+		
+		if choose:
+			print 'choose = ', choose
+			print 'choice = ', choice
+			if choice == 'cancel'	: choose = False
+			if choice == 'more'		: chunk = chunk+1
+			
+			
+			
+			
+		if os.name == 'posix': 
+			tts = True #sys.platform == 'linux-armv71':
+			ttsResponse = 'droid.ttsSpeak(response)'
+			ttsWait = 'while droid.ttsIsSpeaking().result: pass'
+		span = 4; 
+		if tts: 
+			if len(response.split(' ')) > span: 
+				responseList = response.split(' ')
+				responseChunks = [" ".join(responseList[i:i+span]) for i in range(0, len(responseList)-span, span)]
+				response = responseChunks[chunk]
+				#i = 0; chunk = []; j = 0; responseChunks = []
+				#for item in responseList:
+					#if i < 4:
+					#	chunk.append(item); i=i+1
+					#responseChunks.append(chunk)
+				
+			print 'speaking'; 
+			exec(ttsResponse) ; exec(ttsWait) 
+			
+		print "endinput="+input
+		input=""
 
 	dumpFiles() 
 	print dctn	 
 	sys.exit()	   
+
+
+
+
+
+
+
+
+
 
 
 
