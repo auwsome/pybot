bc2924c3f455f750186330422a190176bd2b044a
auwsome <abc@gmail.com>
2015-09-14 11:50:19 -0700

    a

---
+++ pseudocode.py
@@ -0,0 +1,24 @@
+# named as python file for editor functionality, commenting, etc.
+
+write hello
+write 'hello'
+say 'hello'
+write 'hello' into file
+write 'hello' in file named test.txt
+fileName = test.txt
+fileName is test.txt
+write 'how are you?' into file
+
+
+
+dictionary
+tts is true
+dictionary
+
+if I say any string or list, say it
+if I say unknown, ask what is unknown
+if I say search plus something, search something
+
+search cheese
+
+
--- translator.py
+++ translator.py
@@ -5,36 +5,45 @@ from textblob import TextBlob
 blob = TextBlob("write 'hello, how are you?' into a file")
 blob.tags
 
-verbsString = 'write make create'; verbs = verbsString.split(" "); #print verbs
+global functionD; verbsString = 'write,make,create'; verbs = verbsString.split(","); #print verbs
 prepositionsString = 'in into over '; prepositions = prepositionsString.split(" ")
 
 def getInstructions(): with open("pseudocode.py", 'rb') as file: return = file.readlines()
 def remindNoodle(): with open("noodle.txt", 'rb') as file: return = file.readlines()
-def dictionaryNoodle(noodle): 
-	global functionD
-	for line in noodle:
-		if line.startswith("to"):
-			verb = " ".join(line[0:lineList.index(',')-1]); verbs.append(verb)
-			functionS = lineList[lineList.index(','):]; functionL = functionS.split(", "); functionD[verbDef] = functionL
-			return functionD
+def dictionaryNoodle(noodle): for line in noodle: if line.startswith("to"): verbDef = line[:line.index(',')-1]; functionString = line[line.index(',')+1:]; functionD[verbDef] = functionString
 def getInput(): exec('print 1')
 
 def main():	
 	## remind verb/function definitions
 	noodle = remindNoodle()
-	global noodleD; noodleD = dictionaryNoodle(noodle)
+	dictionaryNoodle(noodle); global noodleD; noodleD = functionD
 			
 	## learn instructions
-	for line in getInstructions():
-		print line #'lineList>',lineList
-		lineList = formatLine(line); if lineList == '': break
-		think(lineList)		
-		
+	input = getInstructions()
 	## do input
-	# for line in getInput():
-		# print line #'lineList>',lineList
-		# lineList = formatLine(line); if lineList == '': break
-		# think(lineList)		
+	
+	for line in input:
+		line = formatLine(line); if line == '': break; print line 
+		## check for contractions and split imperative clauses
+		if 'and' or 'then' in line:	
+		lineSplit = line.split("and").split("then")
+		for line in lineSplit:
+		lineList = line.split(" ")## split into list
+		## order of operations
+			## check for conditionals and learn them
+			if "if" in line: 
+				conditionalL = lineList[lineList.index('if'):lineList.index(',')-1]
+				conditionL = conditionalL.remove("if"); 
+					if conditionList[0] == "I":
+						if conditionList[1] == "say":
+							command = joins(lineList[2:lineList.index(',')-1])
+							instructionWhole = line[line.index(',')+1:]
+							if 'then' in instructionWhole = : instructionWhole = instructionWhole.lstrip("then ")
+							noodleD[command] = instructionWhole
+			think(line)	
+		
+	## store noodle	
+	remember(noodle)
 		
 def formatLine(line):
 	## parse line
@@ -45,42 +54,46 @@ def formatLine(line):
 	strings = re.findall(r"\'(.+?)\'",line); #print strings 
 	for index,string in enumerate(strings): ## replace strings with list items
 		line = line.replace(line[line.index(string)-1:line.index(string)+len(string)+1], strings[index]) #print 2,strings[index]
-	lineList = line.split(" "); return LineList #print lineList ## split into list
+	return line 
 
-def think(lineList=lineList):
-		## set variables
-		if ' = ' in line: var = line[:line.index(' = ')]; assignment = line[line.index(' = ')+3:]; global var; var = assignment;
-		if ' is ' in line: var = line[:line.index(' is ')]; assignment = line[line.index(' is ')+3:]; global var; var = assignment; #print var,1,assignment;
-			
-		## order of operations
-		## check for conditionals and store them
-		if "if" in lineList: 
-			if lineList[0] == "if":
-				if lineList[1] == "I":
-					if lineList[2] == "say":
-						command = lineList[2:lineList.index(',')]
-						instructionWhole = lineList[lineList.index(',')+1:]
-						if instructionWhole[0] = 'then': instructionWhole = instructionWhole.remove("then")
-						noodleD[command] = instructionWhole
-		## start with verbs
-		#for verb in verbs:
-			#if verb in lineList:
-		if lineList[0] in verbs:
-			## sentence structure SVO
-			#verbIndex = lineList.index(verb); subject = lineList[:verbIndex]; # print subject ## find if subject
-			if strings: object1 = [strings[0]]
-			else: object1 = lineList[verbIndex+1:] #print subject,object1
-			if verb == 'write': 
-				for preposition in prepositions:
-					if preposition in lineList:
-						#print 3,preposition
-						prepositionIndex = lineList.index(preposition)
-						object2 = lineList[prepositionIndex+1:]; print object2
-						write(object1, preposition, " ".join(object2)); break
-				write(" ".join(object1)); break
-			if lineList[0] in functionD.keys():
-				something = lineList[1:]
+def think(line):
+	lineList = line.split(" "); 
+	## set variables
+	if ' = ' in line: var = line[:line.index(' = ')]; assignment = line[line.index(' = ')+3:]; global var; var = assignment;
+	if ' is ' in line: var = line[:line.index(' is ')]; assignment = line[line.index(' is ')+3:]; global var; var = assignment; #print var,1,assignment;
+		
+	## check for verbs (should be only one per imperative clause)
+	for verb in noodle.keys():
+		if verb.lstrip("to ").strip(" something") in line:
+			function = noodle[verb]; something = something; evaluate(function)
+		
+	if lineList[0] in verbs:
+		## sentence structure SVO
+		#verbIndex = lineList.index(verb); subject = lineList[:verbIndex]; # print subject ## find if subject
+		if strings: object1 = [strings[0]]
+		else: object1 = lineList[verbIndex+1:] #print subject,object1
+		if verb == 'write': 
+			for preposition in prepositions:
+				if preposition in lineList:
+					#print 3,preposition
+					prepositionIndex = lineList.index(preposition)
+					object2 = lineList[prepositionIndex+1:]; print object2
+					write(object1, preposition, " ".join(object2)); break
+			write(" ".join(object1)); break
+		if lineList[0] in functionD.keys():
+			something = lineList[1:]
+
+#def learn(instruction):
+def evaluate():
 	
+	
+def remember(noodle):
+	if functionD != noodleD:
+		for key,value in functionD.items():
+			if functionD[key] not in noodleD.keys():
+				newInstruction = " ".join(['\n', key, value])
+				with open("noodle.txt", 'ab') as file: file.write(newInstruction)
+			elif noodleD[key] != functionD[key]: print 'noodle line not the same'
 	
 def write(sequence, preposition=False, object2=False):
 	fileName=''
@@ -101,18 +114,15 @@ def write(sequence, preposition=False, object2=False):
 			with open(fileName, fileMode) as file: file.write(sequence)
 	else: print sequence #'sequence>',sequence	
 
-def addQuotes(string):
-	return "'"+string+"'"
-def sliceAt(string, slicePoint):
-	return string[string.index(slicePoint)+len(slicePoint):]
-	
-def remember(noodle):
-	if functionD != noodleD:
-		for key,value in functionD.items():
-			for line in getNoodle():
-				if 
+def addQuotes(string): return "'"+string+"'"
+def sliceAt(string, slicePoint): return string[string.index(slicePoint)+len(slicePoint):]
+def joins(list): return " ".join(list)
 	
 # check __main__ to run functions now that defined in any order above
 if __name__=="__main__":
 	main()
 	input = raw_input("")
+	
+'''
+
+'''						
