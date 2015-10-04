3471a99f3d2b3fbf31c86f6e702dadc64ef27140
auwsome <abc@gmail.com>
2015-10-04 12:08:57 -0700

    s

--- pseudocode.py
+++ pseudocode.py
@@ -1,8 +1,16 @@
 	
-exec print(1)
-variable1 = print(1)
-execute variable1
-print 2
+# execute print(1)
+# variable1 = print(1)
+# execute variable1
+
+# print 2+3
+# can't excecute as is with variables because not global(?), unless all on the same line
+# fixed by moving avriable defintions before exec as is, but cannot execute complex (combined) statements
+# so order = replace nouns, execute as is, run as verb definition. Which means
+# a1 = 2; print a1
+a1 = 2 
+print a1
+execute print a1
 
 # print 'hello'
 # write 'hello'
@@ -28,14 +36,20 @@ say 'hello'
 # to scrawl something, write something
 # scrawl 'hey'
 
-# to exit, execute sys.exit()
-# exit
+# to exit, execute sys.exit() # bad keyword, returns exit instructions. same with quit, help
 
+to end, execute sys.exit() 
+# end
+
+save
+
+# another = 'hey'
+# something1 = print(another)
+# execute something1
 
 # to search something, google something, say result
 # search cheese
 # to google something, url is "www.google.com", open url..
-# to quit, exit
 
 
 
@@ -45,9 +59,6 @@ say 'hello'
 
 # shout 'hello'
 
-# another = 'hey'
-# something1 = print(another)
-# exec something1
 
 # write 'hello' into file
 # write 'hello' in file named test.txt
